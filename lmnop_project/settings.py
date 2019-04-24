b339367fd79b961da31248ab40fb06ed42c66251
Nathan W <nathan@email.com>
2019-04-24 16:44:30 -0500

    Added code in note_detail.html for social links and code in views_notes to build the links for posting

--- lmnop_project/settings.py
+++ lmnop_project/settings.py
@@ -38,7 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'lmn'
+    'lmn',
+    'storages',
+    'django_social_share',
 ]
 
 MIDDLEWARE = [
